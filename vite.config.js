a835a46a747e3893d33dd78ada2cb420f7f0136f
Deepu Raneesh <dr833@bath.ac.uk>
2025-04-08 03:50:30 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "dr7",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "dr7",
-    project: "javascript-react"
-  })],
-
-  build: {
-    sourcemap: true
-  }
-})
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/iPhone15_clone",
+});
